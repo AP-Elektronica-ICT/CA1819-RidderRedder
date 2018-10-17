345a1ca92386e490b090909277a6a5b132308659
joeri temmerman <joeri.temmerman@hotmail.com>
2018-10-17 17:06:46 +0200

    first attempt at a working map

--- src/ridder-redder/src/pages/home/home.ts
+++ src/ridder-redder/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  map: GoogleMap;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+    this.map = GoogleMaps.create('map_canvas');
   }
 
 }
