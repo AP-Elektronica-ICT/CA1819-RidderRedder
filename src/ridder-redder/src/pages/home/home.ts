bdd730280bc847f4203bf6c2c106afb8aa83e5a8
joeri temmerman <joeri.temmerman@hotmail.com>
2018-10-19 14:32:31 +0200

    map should work now, static location

--- src/ridder-redder/src/pages/home/home.ts
+++ src/ridder-redder/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapOptions } from '@ionic-native/google-maps';
 
 
 @Component({
@@ -20,7 +20,26 @@ export class HomePage {
   }
 
   loadMap(){
-    this.map = GoogleMaps.create('map_canvas');
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 51.2308134,
+          lng: 4.4238697
+        },
+        zoom: 18,
+        tilt: 30
+      },
+      gestures: {
+        scroll: false,
+        tilt: false,
+        zoom: false,
+        rotate: false
+      },
+    };
+
+    console.log(mapOptions);
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
   }
 
 }
