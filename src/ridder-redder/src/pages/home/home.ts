8063c4564e66e901ca5902f3649d9ad8398ed877
joeri temmerman <joeri.temmerman@hotmail.com>
2018-10-20 16:39:03 +0200

    map updates every 5 seconds, more testing needed

--- src/ridder-redder/src/pages/home/home.ts
+++ src/ridder-redder/src/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, GoogleMapOptions } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapOptions, ILatLng } from '@ionic-native/google-maps';
 import { Geolocation } from '@ionic-native/geolocation';
-
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
 
 
 @Component({
@@ -12,38 +13,53 @@ import { Geolocation } from '@ionic-native/geolocation';
 export class HomePage {
 
   map: GoogleMap;
+  prevPos: ILatLng = { lat: 0, lng: 0 };
+  mapUpdater: Subscription;
+
   constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
 
   ionViewDidLoad(){
     console.log("ionViewDidLoad");
-    this.loadMap();
+    this.mapUpdater = Observable.interval(5000).subscribe(() => {
+      this.loadMap();
+    });
   }
 
   loadMap(){
-    this.geolocation.getCurrentPosition().then(
-      (resp) => {    
-        let mapOptions: GoogleMapOptions = {
-          camera: {
-            target: {
-              lat: resp.coords.latitude,
-              lng: resp.coords.longitude
+    this.geolocation.getCurrentPosition()
+      .then((resp) => {    
+        if (
+          (Math.abs(resp.coords.latitude - this.prevPos.lat) + 
+            Math.abs(resp.coords.longitude - this.prevPos.lng))
+          > 0.0002){      //+- 10 meters difference at 50°N
+
+          let mapOptions: GoogleMapOptions = {
+            camera: {
+              target: {
+                lat: resp.coords.latitude,
+                lng: resp.coords.longitude
+              },
+              zoom: 18,
+              tilt: 30
             },
-            zoom: 18,
-            tilt: 30
-          },
-          gestures: {
-            scroll: false,
-            tilt: false,
-            zoom: false,
-            rotate: false
-          },
-        };
+            gestures: {
+              scroll: false,
+              tilt: false,
+              zoom: false,
+              rotate: false
+            },
+          };
 
-        console.log(mapOptions);
+          console.log(mapOptions);
+          
+          this.prevPos.lat = resp.coords.latitude;
+          this.prevPos.lng = resp.coords.longitude;
 
-        this.map = GoogleMaps.create('map_canvas', mapOptions);
+          this.map = GoogleMaps.create('map_canvas', mapOptions);
+        }
+
       }).catch((error) => {
         console.log(error);
       });
