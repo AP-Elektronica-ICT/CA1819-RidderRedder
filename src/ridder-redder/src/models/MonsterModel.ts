8e57b121277ac293f4ece153df61594c182f55b9
Melvin Bootsgezel <m.bootsgezel@live.nl>
2018-11-05 12:09:55 +0100

    Combat Melee front-end done

---
+++ src/ridder-redder/src/models/MonsterModel.ts
@@ -0,0 +1,4 @@
+export class MonsterModel {
+    MonsterModelId: number;
+    MonsterModelPath: string;
+}
--- src/ridder-redder/src/pages/combat/combat.ts
+++ src/ridder-redder/src/pages/combat/combat.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import { IonicPage, NavController, NavParams, DateTime } from 'ionic-angular';
 import { Monster } from '../../models/Monster';
+import { CombatState } from '../../models/CombatState';
 
 /**
  * Generated class for the CombatPage page.
@@ -16,13 +17,23 @@ import { Monster } from '../../models/Monster';
 })
 export class CombatPage {
 
-    private monster1: Monster;
-    private numbers;
+    @Input() monster: Monster;
+
+    private combatState: CombatState = CombatState.ChoosingCombatStyle;
+    private infoHead: string;
+    private infoParagraph: string;
+
+    private timer: number;
+    private maxTime: number;
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
-        this.monster1 = {
+        this.monster = {
             Difficulty: Math.floor(Math.random() * 4) + 1,
             Level: 5,
+            Model: {
+                MonsterModelId: 1,
+                MonsterModelPath: ""
+            },
             Name: {
                 MonsterName: "Charles",
                 MonsterNameId: 1
@@ -32,6 +43,9 @@ export class CombatPage {
                 MonsterTitleId: 1
             }
         }
+        this.setInfo();
+
+        this.maxTime = 120 / this.monster.Difficulty;
     }
 
     ionViewDidLoad() {
@@ -40,6 +54,63 @@ export class CombatPage {
 
     difficulty(n: number): any[] {
         return Array(n);
+    }
+
+    setInfo() {
+        switch (this.combatState) {
+            case CombatState.ChoosingCombatStyle:
+                this.infoHead = "Battle time!";
+                this.infoParagraph = "Choose a combat style to start the fight";
+                break;
+            case CombatState.CombatMelee:
+                this.infoHead = "Slash him!";
+                this.infoParagraph = "Slash your phone towards the monster to damage him";
+                break;
+            case CombatState.CombatRanged:
+                this.infoHead = "Shoot him!"
+                this.infoParagraph = "Aim and drag the bow to shoot";
+                break;
+            case CombatState.CombatMagic:
+                this.infoHead = "Hocus Pocus"; //Replace with magic spell
+                this.infoParagraph = "Speak up! Call out this spell to deal damage";
+                break;
+        }
+    }
+
+    startCombat() {
+        console.log("Player has selected combat style, starting combat");
+        this.startTimer();
+    }
+
+    selectCombatStyle(e) {
+        switch (e.path[0].id) {
+            case "img-sword":
+                this.combatState = CombatState.CombatMelee;
+                break;
+            case "img-bow":
+                this.combatState = CombatState.CombatRanged;
+                break;
+            case "img-wand":
+                this.combatState = CombatState.CombatMagic;
+                break;
+        }
+        this.setInfo();
+        this.startCombat();
+    }
+
+    startTimer() {
+        this.timer = setTimeout(x => {
+            if (this.maxTime <= 0) {
+                
+            }
+            this.maxTime -= 1;
+
+            if (this.maxTime > 0) {
+                this.startTimer();
+            }
+
+        }, 1000);
+
     }
 
 }
