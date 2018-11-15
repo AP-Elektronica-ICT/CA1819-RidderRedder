cc00221f2c1dce9001103ab9db4d040013b1a355
Melvin Bootsgezel <m.bootsgezel@live.nl>
2018-11-15 13:17:34 +0100

    WebAPI initialised with Swagger documentation

---
+++ src/RidderRedderApi/RidderRedderApi/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace RidderRedderApi.Controllers {
+
+    public class BaseController : Controller {
+        public BaseController() {
+
+        }
+    }
+}
---
+++ src/RidderRedderApi/RidderRedderApi/Controllers/MonsterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RidderRedderApi.Controllers {
+
+    /// <summary>
+    /// Monster controller.
+    /// </summary>
+    [Route("api/v1/[controller]")]
+    public class MonsterController : BaseController {
+
+        //private readonly
+
+        /// <summary>
+        /// Gets the monsters.
+        /// </summary>
+        /// <returns>The monsters.</returns>
+        [HttpGet]
+        public IActionResult GetMonsters() {
+            return Ok("Monster");
+        }
+
+    }
+}
