18e0cd9ee0cdda13199847fbcb5c0fcbdb9e8855
Melvin Bootsgezel <m.bootsgezel@live.nl>
2018-12-19 21:15:45 +0100

    Login page opacity & transition fixed

--- src/ridder-redder/src/pages/login/login.ts
+++ src/ridder-redder/src/pages/login/login.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/AuthProvider';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -16,12 +17,37 @@ import { AuthProvider } from '../../providers/auth/AuthProvider';
 })
 export class LoginPage {
 
+    loginPageContent: HTMLElement;
+    loginContainer: HTMLElement;
+
     constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthProvider) {
 
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad LoginPage');
+
+        this.loginPageContent = document.getElementById('loginPageContent');
+        this.loginContainer = document.getElementById('loginContainer');
+    }
+
+    startGame() {
+        // TODO: navigate to Home page
+        if(this.loginContainer)
+            this.loginContainer.style.opacity = "0";
+        if(this.loginPageContent)
+            this.loginPageContent.style.opacity = "1";
+
+        setTimeout(() => {
+            this.navCtrl.push(HomePage).then(() => {
+                this.loginPageContent.style.opacity = "0.85";
+                this.loginContainer.style.opacity = "1";
+            });
+        }, 2500);
+    }
+
+    showOptions() {
+        // TODO: navigate to Options page
     }
 
 }
